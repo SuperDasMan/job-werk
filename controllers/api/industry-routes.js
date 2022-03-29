9e55698a32ced9643d2c84a91c5dfec20807c38b
Brandonpeterson6 <brandonpeterson.006@gmail.com>
2022-03-29 11:58:01 -0600

    working on routes more

--- controllers/api/industry-routes.js
+++ controllers/api/industry-routes.js
@@ -1,36 +1,61 @@
 const router = require("express").Router();
-const sequelize = require("../../config/connection");
-const { Industry } = require("../../models");
+const { Industry, Job } = require("../../models");
 
 // get all industries
-router.get("/", (req, res) => {
-    console.log("======================");
-    Industry.findAll({
-
-    
-    })
-      .then((dbPostData) => res.json(dbPostData))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
+router.get('/', (req, res) => {
+  Industry.findAll({
+    attributes: [
+      'id',
+      'name'
+    ],
+    include: [
+      {
+        model: Job,
+        attributes: [
+          "id",
+          "job_url",
+          "title",
+          "created_at"
+        ],
+      },
+    ]
+  })
+    .then(dbIndustryData => res.json(dbIndustryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
   // get one industry
-  router.get("/:id", (req, res) => {
+  router.get('/:id', (req, res) => {
     Industry.findOne({
-
-
-     
+      attributes: [
+        'id',
+        'name'
+      ],
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Job,
+          attributes: [
+            "id",
+          "job_url",
+          "title",
+          "created_at"
+          ],
+        },
+      ]
     })
-      .then((dbPostData) => {
-        if (!dbPostData) {
-          res.status(404).json({ message: "No Industry found with this id" });
+      .then(dbIndustryData => {
+        if (!dbIndustryData) {
+          res.status(404).json({ message: 'No Industry found with this id' });
           return;
         }
-        res.json(dbPostData);
+        res.json(dbIndustryData);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
--- controllers/api/job-routes.js
+++ controllers/api/job-routes.js
@@ -1,5 +1,44 @@
 const router = require("express").Router();
-const sequelize = require("../../config/connection");
+const { Industry, Job, Vote } = require('../../models');
 
 
+// get all jobs
+router.get('/', (req, res) => {
+
+    Job.findAll({
+      include: [
+        { model: Industry },
+        { model: Job }
+      ]
+    })
+      .then(dbJobData => res.json(dbJobData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+// get one job
+router.get('/:id', (req, res) => {
+  
+    Product.findOne({
+      where: { id: req.params.id },
+      include: [
+        { model: Industry },
+        { model: Job }
+      ]
+    })
+      .then(dbJobData => {
+        if (!dbJobData) {
+          res.status(404).json({ message: 'No job found with this id' });
+          return;
+        }
+        res.json(dbJobData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    });
+    
 module.exports = router;
