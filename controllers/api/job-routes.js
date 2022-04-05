8baa201f22e0a751d49242383e749e6ea403ebee
Josh West <bigdaddydas@gmail.com>
2022-04-05 10:08:29 -0600

    smash together for collab

--- controllers/api/job-routes.js
+++ controllers/api/job-routes.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
-const { Industry, Job, Vote } = require('../../models');
+const { User, Industry, Job, Vote } = require('../../models');
 
 // get all jobs
 router.get('/', (req, res) => {
   Job.findAll({
-    include: [{ model: Industry }, { model: Job }],
+    attributes: [
+      'id',
+      'job_url',
+      'title',
+      'created_at',
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM vote WHERE job.id = vote.job_id)'
+        ),
+        'vote_count',
+      ],
+    ],
+    include: [
+      { model: Industry, attributes: ['name'] },
+      { model: User, attributes: ['username'] },
+    ],
   })
     .then((dbJobData) => res.json(dbJobData))
     .catch((err) => {
@@ -17,7 +32,23 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   Job.findOne({
     where: { id: req.params.id },
-    include: [{ model: Industry }, { model: Job }],
+
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+        ),
+        'vote_count',
+      ],
+    ],
+    include: [
+      { model: Industry, attributes: ['name'] },
+      { model: User, attributes: ['username'] },
+    ],
   })
     .then((dbJobData) => {
       if (!dbJobData) {
@@ -25,6 +56,37 @@ router.get('/:id', (req, res) => {
         return;
       }
       res.json(dbJobData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// post a job
+router.post('/', (req, res) => {
+  Job.create({
+    title: req.body.title,
+    description: req.body.description,
+    pay_rate: req.body.pay_rate,
+    job_url: req.body.job_url,
+    industry_id: req.body.industry_id,
+    include: [
+      { model: Industry, attributes: ['name'] },
+      { model: User, attributes: ['username'] },
+    ],
+  })
+    .then((dbJobData) => {
+      req.session.save(() => {
+        req.session.job_id = dbJobData.id;
+        req.session.title = dbJobData.title;
+        req.session.description = dbJobData.description;
+        req.session.pay_rate = dbJobData.pay_rate;
+        req.session.industry_id = dbJobData;
+        req.session.loggedIn = true;
+      });
+
+      res.json({ job: dbJobData, message: 'New Job successfully posted!' });
     })
     .catch((err) => {
       console.log(err);
--- models/Job.js
+++ models/Job.js
@@ -17,7 +17,7 @@ class Job extends Model {
           'job_url',
           'title',
           'pay_rate',
-          // "created_at",
+          'created_at',
           [
             sequelize.literal(
               '(SELECT COUNT(*) FROM vote WHERE job.id = vote.job_id)'
@@ -86,6 +86,7 @@ Job.init(
   },
   {
     sequelize,
+    timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'job',
